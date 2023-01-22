22dc4625940e1d0b51f09de472293294a9df8c74
Rishabh Rawat <rishabh2001rawat@gmail.com>
2023-01-22 19:44:49 +0530

    new new

--- react01/src/components/about/About.jsx
+++ react01/src/components/about/About.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import Nav from '../navbar/Nav'
+import aboutimg  from '../../assets/trafalgar-header illustration 1.png';
+import '../about/about.css'
 
 function About() {
   return (
     <div>
-      <Nav />
+      <Nav/>
+
+      <div className='about-title'>
+        <div className='left-box'>
+        <h1>Virtual healthcare for you</h1>
+        <p>Trafalgar provides progressive, and affordable healthcare, accessible on mobile and online for everyone</p>
+        <button type="button">Consult today</button>
+        </div>
+      
+      <div className='right-box'>
+        <img src={aboutimg} alt="" />
+        </div>
+      </div>
+
     </div>
   )
 }
--- react01/src/components/navbar/Nav.jsx
+++ react01/src/components/navbar/Nav.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import Logo  from '../../assets/logo.png';
+import Logo from '../../assets/logo.png';
 import '../navbar/nav.css'
 
 function Nav() {
   return (
     <div className='navbar'>
-        <div className="left-div"><img src={Logo} alt="app logo" /></div>
-        <div className="right-div">
-            <ul className='nav-item'>
-                <li>Home </li>
-                <li>About </li>
-                <li>App </li>
-                <li>Testimonials </li>
-                <li>Contact us </li>
-            </ul>
-        </div>
+      <div className="left-div"><img src={Logo} alt="app logo" /></div>
+      <div className="right-div">
+        <ul className='nav-item'>
+          <li> <a href="http://www.google.com">Home</a>  </li>
+          <li> <a href="">About</a>  </li>
+          <li><a href="">App</a>  </li>
+          <li><a href="">Testimonials</a>  </li>
+          <li><a href="">Contact us</a>  </li>
+        </ul>
+      </div>
     </div>
   )
 }
